b17fa5b4af6404f131fa22eb69caa6cd6726e506
Your Name <you@example.com>
2022-09-09 19:48:06 +1000

    add edit rendering

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Project from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -8,10 +8,18 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
+    const [projectBeingEdited, setProjectBeingEdited] = useState({});
+    const handleEdit = (project: Project) => {
+        setProjectBeingEdited(project);
+    };
     const items = projects.map(project => (
         <div key={project.id} className="cols-sm">
-            <ProjectCard project={project}></ProjectCard>
-            <ProjectForm />
+            {projectBeingEdited === project ? (
+                <ProjectForm />
+            ) : ( 
+                <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
+            )
+            }           
         </div>
     ) )
     return <div className="row">{items}</div>
