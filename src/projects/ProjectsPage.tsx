e19324d5f523658fa4f5defd1df69d72b897e14d
Your Name <you@example.com>
2022-09-13 16:45:47 +1000

    API & calls setup

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,54 @@
-import React, { Fragment, useState } from 'react';
-import { MOCK_PROJECTS } from './MockProjects';
+import React, { Fragment, useEffect, useState } from 'react';
+// import { MOCK_PROJECTS } from './MockProjects';
 import ProjectList from './ProjectList';
 import Project from './Project';
+import { projectAPI } from './projectAPI';
 
 function ProjectsPage() {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const handleMoreClick = () => {
+        setCurrentPage((currentPage) => currentPage + 1);
+    }
+
+    useEffect(() => {
+        // setLoading(true);
+        // projectAPI.get(1)
+        //     .then(setProjects)
+        //     .then(() => {
+        //         setLoading(false);
+        //     })
+        //     .catch((error) => {
+        //         setError(error);
+        //         setLoading(false);
+        //     });
+        async function loadProjects() {
+            setLoading(true);
+            try {
+                const data = await projectAPI.get(currentPage);
+                setError('');
+                if (currentPage === 1) {
+                    setProjects(data);
+                } else {
+                    setProjects((projects) => [...projects, ...data]);
+                }
+
+            }
+            catch (e) {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadProjects();
+    }, [currentPage])
+
+
     const saveProject = (project: Project) => {
         let updatedProjects = projects.map((p: Project) => {
             return p.id === project.id ? project : p;
@@ -13,10 +57,40 @@ function ProjectsPage() {
     };
     return (
         <Fragment>
-            <h1>Projects</h1>       
+            <h1>Projects</h1> 
+            {error && (
+                <div className="row">
+                    <div className='card large error'>
+                        <section>
+                            <p>
+                                <span className='icon-alert inverse '></span>
+                                {error}
+                            </p>
+                        </section>
+                    </div>
+
+                </div>
+            )}      
             <ProjectList 
                 projects={projects} 
                 onSave={saveProject} />
+            {!loading && !error && (
+                <div className='row'>
+                    <div className='col-sm-12'>
+                        <div className='button-group fluid'>
+                            <button className='button default' onClick={handleMoreClick}>
+                                More...
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {loading && (
+            <div className="center-page">
+                <span className="spinner primary"></span>
+                 <p>Loading...</p>
+            </div>
+            )}
         </Fragment>
     );  
 }
