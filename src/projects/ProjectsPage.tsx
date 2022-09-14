d4d7c5ab796bb3172fd8f1e34cdb1c59ddbb9a18
Your Name <you@example.com>
2022-09-14 16:29:04 +1000

    add updating of the persistent state of projects via api

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -50,10 +50,19 @@ function ProjectsPage() {
 
 
     const saveProject = (project: Project) => {
-        let updatedProjects = projects.map((p: Project) => {
-            return p.id === project.id ? project : p;
-        });
-        setProjects(updatedProjects);
+        projectAPI
+            .put(project)
+            .then((updatedProject) => {
+                let updatedProjects = projects.map((p: Project) => {
+                    return p.id === project.id ? new Project(updatedProject) : p;             
+                });
+                setProjects(updatedProjects);
+            } )
+            .catch((e) => {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            });
     };
     return (
         <Fragment>
